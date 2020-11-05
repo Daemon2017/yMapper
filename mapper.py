d53b6fe9d7cd23afe84887e34e995c741f91b766
Dmitrij Rusakov <daemon2017@gmail.com>
2020-11-05 10:03:08 +0300

    Добавлена возможность конвейерной обработки списка SNP

--- mapper.py
+++ mapper.py
@@ -7,9 +7,9 @@ from multiprocessing import freeze_support
 # * False - если хотим работать только с теми данными, что 100% подтверждены BigY500/BigY700/SNP;
 # * True - если хотим работать с наибольшим возможным количеством данных, полученным в результате предсказания SNP по
 #  STR с некоторой точностью.
-is_extended = True
+is_extended = False
 # Задаем целевой SNP, чьи дочерние SNP будут наноситься на карту.
-target_snp = 'R-CTS1211'
+target_snps = ['R-CTS1211']
 # Задаем количество STR (12/37/67/111), которое будет использоваться на шаге предсказания SNP.
 str_number = 111
 # Задаем координаты левого нижнего угла сетки из шестиугольников.
@@ -29,27 +29,30 @@ is_web = True
 
 if __name__ == '__main__':
     freeze_support()
-    print(datetime.datetime.now())
-    combined_original_df = utils.get_combined_df()
-    json_tree_rows = utils.get_json_tree_rows()
-    child_snps = utils.get_child_snps(json_tree_rows, target_snp)
-    combination_to_color_dict = utils.get_combination_to_color_dict(child_snps)
 
-    if is_web:
-        x_0 = -180
-        y_0 = -90
-        x_1 = 360
-        y_1 = 180
-        h_list = [1.0]
-    polygon_list_list = utils.get_polygon_list_list(h_list, y_0, y_1, x_0, x_1)
+    for target_snp in target_snps:
+        print("Обрабатывается SNP {}...".format(target_snp))
+        print(datetime.datetime.now())
+        combined_original_df = utils.get_combined_df()
+        json_tree_rows = utils.get_json_tree_rows()
+        child_snps = utils.get_child_snps(json_tree_rows, target_snp)
+        combination_to_color_dict = utils.get_combination_to_color_dict(child_snps)
 
-    combined_normal_df_positive_snps = utils.get_df_positive_snps(child_snps, combined_original_df, json_tree_rows)
-    combined_normal_df_without_other = utils.get_df_without_other(combined_normal_df_positive_snps)
-    utils.get_map(combined_normal_df_without_other.copy(), False, polygon_list_list, child_snps, y_center, x_center,
-                  zoom, combination_to_color_dict, target_snp, h_list, is_web)
-    if is_extended:
-        combined_extended_df = utils.get_df_extended(combined_normal_df_without_other, str_number, json_tree_rows,
-                                                     child_snps, combined_original_df)
-        utils.get_map(combined_extended_df, True, polygon_list_list, child_snps, y_center, x_center, zoom,
-                      combination_to_color_dict, target_snp, h_list, is_web)
-    print(datetime.datetime.now())
+        if is_web:
+            x_0 = -180
+            y_0 = -90
+            x_1 = 360
+            y_1 = 180
+            h_list = [1.0]
+        polygon_list_list = utils.get_polygon_list_list(h_list, y_0, y_1, x_0, x_1)
+
+        combined_normal_df_positive_snps = utils.get_df_positive_snps(child_snps, combined_original_df, json_tree_rows)
+        combined_normal_df_without_other = utils.get_df_without_other(combined_normal_df_positive_snps)
+        utils.get_map(combined_normal_df_without_other.copy(), False, polygon_list_list, child_snps, y_center, x_center,
+                      zoom, combination_to_color_dict, target_snp, h_list, is_web)
+        if is_extended:
+            combined_extended_df = utils.get_df_extended(combined_normal_df_without_other, str_number, json_tree_rows,
+                                                         child_snps, combined_original_df)
+            utils.get_map(combined_extended_df, True, polygon_list_list, child_snps, y_center, x_center, zoom,
+                          combination_to_color_dict, target_snp, h_list, is_web)
+        print(datetime.datetime.now())
