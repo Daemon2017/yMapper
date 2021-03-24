d75779ff15afe033d826110afd30687b99dcf5e3
Dmitrij Rusakov <daemon2017@gmail.com>
2021-03-24 22:34:53 +0300

    Добавлена возможность получения списка всех потомков выбранного SNP

---
+++ get_list_of_descendants.py
@@ -0,0 +1,20 @@
+import json
+
+import ftdna_tree_collector_rest
+import utils
+
+
+def get_list_of_descendants(start):
+    stack = [start]
+    snps = []
+    while stack:
+        current_snp = stack.pop()
+        snps.append(current_snp)
+        stack.extend(snp for snp in ftdna_tree_collector_rest.get_children_list(json_tree_rows, current_snp))
+    return snps
+
+
+json_tree_rows = utils.get_json_tree_rows()
+target_snp = "R-Z92"
+list_of_descendants = get_list_of_descendants(target_snp)
+print(json.dumps(list_of_descendants))
