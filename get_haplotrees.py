361ca5b0ae0455d187300e51f60e8ce8ac154f56
Dmitrij Rusakov <daemon2017@gmail.com>
2021-09-23 23:12:31 +0300

    Древо сделано единым для всех ГГ Древо хранится частями по 10к SNP

--- get_haplotrees.py
+++ get_haplotrees.py
@@ -1,35 +1,39 @@
 import json
+from itertools import islice
 
 import firebase_admin
 from firebase_admin import credentials, firestore
 
 import utils
 
+
+def chunks(data, size):
+    it = iter(data)
+    for i in range(0, len(data), size):
+        yield {k: data[k] for k in islice(it, size)}
+
+
 json_tree_rows = utils.get_json_tree_rows()
 
-for i in range(ord('A'), ord('T') + 1):
-    haplogroup = chr(i)
-    print("Обрабатывается ГГ {}...".format(haplogroup))
+new_all_nodes = {}
+for node in json_tree_rows['allNodes']:
+    new_all_nodes[node] = {
+        'name': json_tree_rows['allNodes'][node]['name']
+    }
+    if 'children' in json_tree_rows['allNodes'][node]:
+        new_all_nodes[node]['children'] = json_tree_rows['allNodes'][node]['children']
+    if 'parentId' in json_tree_rows['allNodes'][node]:
+        new_all_nodes[node]['parentId'] = json_tree_rows['allNodes'][node]['parentId']
 
-    new_all_nodes = {}
-    for node in json_tree_rows['allNodes']:
-        if json_tree_rows['allNodes'][node]['name'].startswith('{}-'.format(haplogroup)):
-            new_all_nodes[node] = {
-                'name': json_tree_rows['allNodes'][node]['name']
-            }
-            if 'children' in json_tree_rows['allNodes'][node]:
-                new_all_nodes[node]['children'] = json_tree_rows['allNodes'][node]['children']
-            if 'parentId' in json_tree_rows['allNodes'][node]:
-                new_all_nodes[node]['parentId'] = json_tree_rows['allNodes'][node]['parentId']
+if not firebase_admin._apps:
+    cred = credentials.Certificate('serviceAccount.json')
+    firebase_admin.initialize_app(cred)
+db = firestore.client()
 
-    new_json_tree_rows = {'allNodes': new_all_nodes}
-
-    if not firebase_admin._apps:
-        cred = credentials.Certificate('serviceAccount.json')
-        firebase_admin.initialize_app(cred)
-    db = firestore.client()
-
-    doc_ref = db.collection("haplotrees").document(haplogroup)
+i = 0
+for item in chunks(new_all_nodes, 10000):
+    doc_ref = db.collection("haplotree").document("part" + str(i))
     doc_ref.set({
-        u'data': json.dumps(new_json_tree_rows)
+        u'data': json.dumps(item)
     })
+    i = i + 1
